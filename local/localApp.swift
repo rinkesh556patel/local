b12c093092d7eb85653f99390176930b7c694347
“rinkesh556patel” <rinkeshpatel556@gmail.com>
2024-12-07 12:06:08 +0530

    Initial Commit

---
+++ local/localApp.swift
@@ -0,0 +1,17 @@
+//
+//  localApp.swift
+//  local
+//
+//  Created by Rinkesh Patel on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct localApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ localTests/localTests.swift
@@ -0,0 +1,16 @@
+//
+//  localTests.swift
+//  localTests
+//
+//  Created by Rinkesh Patel on 07/12/24.
+//
+
+import Testing
+
+struct localTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
